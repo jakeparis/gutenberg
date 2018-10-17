f864dc02db60897cb8affa12468e357512830952
Robert Anderson <robert@noisysocks.com>
2018-10-17 16:56:11 +1100

    First pass at adding 'Advanced Panels' to Options modal
    Allows the user to enable or disable 'Advanced Panels' (AKA Meta Boxes)
    via the Options modal.
    
    - The title and ID of each Meta Box is stored in the `core/edit-post`
      Redux store.
    - The `MetaBoxTitles` HOC provides an easy way to list all of the
      registered Meta Boxes within Gutenberg.
    - The `MetaBoxesVisibility` and `MetaBoxVisibility` components respond
      to the existing `isEditorPanelEnabled` selector and show/hide Meta Boxes
      using `el.style.display = 'none'`;

---
+++ packages/edit-post/src/components/meta-boxes-visibility/meta-box-visibility.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+import { withSelect } from '@wordpress/data';
+
+class MetaBoxVisibility extends Component {
+	componentDidMount() {
+		this.updateDOM();
+	}
+
+	componentDidUpdate( prevProps ) {
+		if ( this.props.isVisible !== prevProps.isVisible ) {
+			this.updateDOM();
+		}
+	}
+
+	updateDOM() {
+		const { id, isVisible } = this.props;
+		document.getElementById( id ).style.display = isVisible ? '' : 'none';
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default withSelect( ( select, { id } ) => ( {
+	isVisible: select( 'core/edit-post' ).isEditorPanelEnabled( `meta-box-${ id }` ),
+} ) )( MetaBoxVisibility );
